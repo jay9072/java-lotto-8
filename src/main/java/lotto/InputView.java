4e0ecc11bfafa9303706cb8bcb7483020f096824
jay9072 <jay907212@gmail.com>
2025-11-03 20:58:25 +0900

    feat: 사용자 입력 및 출력 기능 구현

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,4 +1,38 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public static int readPurchaseAmount(){
+        System.out.println("구입금액을 입력해 주세요 ");
+        String input = Console.readLine().trim();
+        int amount = parseAmount(input);
+        return amount;
+    }
+
+    //당첨 번호 입력
+    public static String readWinningNumbers(){
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        return Console.readLine().trim();
+    }
+
+    //보너스 번호 입력
+    public static String readBonusNumbers(){
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        return Console.readLine().trim();
+    }
+
+    //내부 유효성 검사
+    private static int parseAmount(String input) {
+        try {
+            int amount = Integer.parseInt(input);
+            if (amount <= 0 || amount % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위의 양수여야 합니다.");
+            }
+            return amount;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR 숫자를 입력해야 합니다");
+        }
+    }
 }
--- src/main/java/lotto/OutView.java
+++ src/main/java/lotto/OutView.java
@@ -1,4 +1,31 @@
 package lotto;
 
-public class OutView {
+import java.util.List;
+
+
+public class OutputView {
+
+    // 구매한 로또 목록 출력
+    public static void printLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers()); // Lotto의 번호 리스트 출력
+        }
+        System.out.println();
+    }
+
+    // 당첨 결과 출력
+    public static void printResult(List<String> resultLines, double rate) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (String line : resultLines) {
+            System.out.println(line);
+        }
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", rate);
+    }
+
+    // 에러 메시지 출력
+    public static void printError(String message) {
+        System.out.println(message);
+    }
 }
