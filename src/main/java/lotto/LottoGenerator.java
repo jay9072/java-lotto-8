b1215374491e5dfc22a3e7ff97602c1c1d018784
jay9072 <jay907212@gmail.com>
2025-11-03 20:57:42 +0900

    feat: 로또 번호 자동 생성 기능 구현

--- src/main/java/lotto/LottoGenerator.java
+++ src/main/java/lotto/LottoGenerator.java
@@ -1,4 +1,17 @@
 package lotto;
 
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+/**
+ * LottoGenerator 클래스는 랜덤한 6개의 로또 번호를 생성하는 역할을 함
+ */
+
 public class LottoGenerator {
+
+    public static List<Integer> generate(){
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
 }
