6061351de52a2e984133a704f7cc1886fcc193fa
jay9072 <jay907212@gmail.com>
2025-11-03 20:58:03 +0900

    feat: 구입 금액에 따른 로또 여러 장 발행 기능 구현

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -1,4 +1,28 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoStore {
+
+    /**
+     * LottoStore 클래스는 사용자가 입력한 금액에 따라
+     * 여러 장의 로또를 발행하는 역할을 담당한다
+     */
+
+    private static final int LOTTO_PRICE = 1000;
+
+    public static List<Lotto> buyLottos(int amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위어야 합니다.");
+        }
+
+        int count = amount / LOTTO_PRICE;
+        List<Lotto> lottos = new ArrayList<>();
+
+        for (int i = 0; i <count; i++) {
+            lottos.add(new Lotto(LottoGenerator.generate()));
+        }
+        return lottos;
+    }
 }
