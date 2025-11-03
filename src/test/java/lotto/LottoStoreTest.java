5bad090c35100a7c6088eff80c19f6d3df007bf1
jay9072 <jay907212@gmail.com>
2025-11-03 20:58:12 +0900

    test: 로또 발행 수량 및 예외 처리 테스트 추가

--- src/test/java/lotto/LottoStoreTest.java
+++ src/test/java/lotto/LottoStoreTest.java
@@ -1,4 +1,33 @@
 package lotto;
 
-public class LottoStoreTest {
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+
+class LottoStoreTest {
+
+    @Test
+    void 금액에_맞게_로또를_발행한다() {
+        // given
+        int amount = 5000;
+
+        // when
+        List<Lotto> lottos = LottoStore.buyLottos(amount);
+
+        // then
+        assertThat(lottos).hasSize(5); // 5000원이면 5장 발행되어야 함
+    }
+
+    @Test
+    void 금액이_1000원_단위가_아니면_예외_발생() {
+        // given
+        int wrongAmount = 550;
+
+        // when & then
+        assertThatThrownBy(() -> LottoStore.buyLottos(wrongAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
 }
